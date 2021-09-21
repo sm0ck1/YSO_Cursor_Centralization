41cfb25426184b49b226ee60697e5c72cdf1a721
ser-yehorov <Em73Sz63>
2021-09-21 16:49:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DESCRIPTION
+Center the cursor on the active window
+
+# INSTALL
+```npm i```
+
+```node main```
+
+# WARNING
+Only tested on Windows 10
---
+++ main.js
@@ -0,0 +1,64 @@
+const ffi = require('ffi-napi');
+const repbuffer = Buffer.alloc(16);
+
+const user32 = new ffi.Library('user32', {
+    'GetForegroundWindow': ['long', []],
+    'GetWindowRect': ['bool', ['long', 'pointer']],
+    'SetCursorPos': ['long', ['long', 'long']],
+    'GetCursorPos': ['bool', ['pointer']]
+});
+
+// create rectangle from pointer
+const pointerToRect = function(rectPointer) {
+    const rect = {};
+    rect.left = rectPointer.readInt16LE(0);
+    rect.top = rectPointer.readInt16LE(4);
+    rect.right = rectPointer.readInt16LE(8);
+    rect.bottom = rectPointer.readInt16LE(12);
+    return rect;
+}
+
+function getmousepos() {
+    user32.GetCursorPos(repbuffer);
+    let pos = {};
+    pos.x = repbuffer[0] + (repbuffer[1] * 256);
+    pos.y = repbuffer[4] + (repbuffer[5] * 256);
+    return pos;
+    А
+}
+
+// obtain window dimension
+const getWindowDimensions = function(handle) {
+    const rectPointer = Buffer.alloc(16);
+    const getWindowRect = user32.GetWindowRect(handle, rectPointer);
+    return !getWindowRect ?
+        null :
+        pointerToRect(rectPointer);
+}
+
+let currentActiveWindow = 0;
+
+setInterval(() => {
+    try {
+        const activeWindow = user32.GetForegroundWindow();
+        const activeWindowDimensions = getWindowDimensions(activeWindow);
+
+        if (activeWindowDimensions.right === 'null') return;
+
+        const activeWindowWidth = activeWindowDimensions.right - activeWindowDimensions.left;
+        const activeWindowHeight = activeWindowDimensions.bottom - activeWindowDimensions.top;
+
+        const mouse = getmousepos();
+
+        if (!(mouse.x < activeWindowDimensions.right && mouse.x > activeWindowDimensions.left) &&
+            activeWindow !== currentActiveWindow) {
+
+            user32.SetCursorPos(activeWindowDimensions.right - (activeWindowWidth / 2), activeWindowDimensions.bottom - (activeWindowHeight / 2));
+        }
+
+        currentActiveWindow = activeWindow;
+
+    } catch (error) {
+        console.log(error);
+    }
+}, 100);
